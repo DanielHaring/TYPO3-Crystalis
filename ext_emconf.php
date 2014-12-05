d5cbafe02050722b1d1eefacd4ce8faaa57d5566
Daniel Haring <daniel.haring@cocowerbung.at>
2014-12-05 10:14:29 +0100

    [TASK] Increase TYPO3 CMS version compatibility to 7.0.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,9 +30,9 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
                         'php' => '5.4.0-5.6.99',
-			'extbase' => '6.2.0-6.2.99',
-			'fluid' => '6.2.0-6.2.99',
-			'typo3' => '6.2.0-6.2.99',
+			'extbase' => '6.2.0-7.0.99',
+			'fluid' => '6.2.0-7.0.99',
+			'typo3' => '6.2.0-7.0.99',
 		),
 		'conflicts' => 
 		array (
