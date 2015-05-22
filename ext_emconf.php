873c8d70c4ca779d405f996de911c20ff044dce9
Daniel Haring <development@haring.co.at>
2015-05-23 00:03:40 +0200

    [TASK] Set Crystalis version to 7.2.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array (
         'description' => 'A flexible headstone for professional next generation TYPO3 CMS websites. ' 
                             . 'Crystalis provides content rendering, pre-configured setups, fully automatic language handling and many more. ',
 	'category' => 'fe',
-	'version' => '7.1.0-dev',
+	'version' => '7.2.0-dev',
 	'state' => 'beta',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -27,9 +27,9 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'extbase' => '7.1.0-7.1.99',
-			'fluid' => '7.1.0-7.1.99',
-			'typo3' => '7.1.0-7.1.99',
+			'extbase' => '7.2.0-7.2.99',
+			'fluid' => '7.2.0-7.2.99',
+			'typo3' => '7.2.0-7.2.99',
 		),
 		'conflicts' => 
 		array (
