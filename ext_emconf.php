8b2c741ffd20954ecd9613af8696345d02f79437
Daniel Haring <development@haring.co.at>
2015-06-04 14:50:25 +0200

    [TASK] Set Crystalis state to 'stable'
    Releases: 7.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array (
                             . 'Crystalis provides content rendering, pre-configured setups, fully automatic language handling and many more. ',
 	'category' => 'fe',
 	'version' => '7.2.0-dev',
-	'state' => 'beta',
+	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'clearCacheOnLoad' => 1,
