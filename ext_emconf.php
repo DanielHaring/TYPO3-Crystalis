1f797ade4bea799b6ec33bbfa664945441231477
Daniel Haring <development@haring.co.at>
2015-09-24 18:48:15 +0200

    [RELEASE] Release of Crystalis 7.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array (
         'description' => 'A flexible headstone for professional next generation TYPO3 CMS websites. ' 
                             . 'Crystalis provides content rendering, pre-configured setups, fully automatic language handling and many more. ',
 	'category' => 'fe',
-	'version' => '7.4.0-dev',
+	'version' => '7.4.0',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
