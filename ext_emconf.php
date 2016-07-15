a46e5de68fd52b1ba1052defacb96f5370f6e99b
Daniel Haring <development@haring.co.at>
2016-07-15 22:16:27 +0200

    [TASK] Upgrade to TYPO3 CMS v8.2.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,9 +26,9 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' =>
 		array (
-			'extbase' => '8.1.0-8.1.99',
-			'fluid' => '8.1.0-8.1.99',
-			'typo3' => '8.1.0-8.1.99',
+			'extbase' => '8.2.0-8.2.99',
+			'fluid' => '8.2.0-8.2.99',
+			'typo3' => '8.2.0-8.2.99',
 		),
 		'conflicts' =>
 		array (
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'suggests' =>
 		array (
             'realurl' => '1.13.4-2.0.99',
-            'felogin' => '8.1.0-8.1.99'
+            'felogin' => '8.2.0-8.2.99'
 		),
 	),
 );
